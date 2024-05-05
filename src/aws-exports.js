5db979b29c2ce3002e4ead967ff4a051ea73678b
Ubuntu <ubuntu@ip-10-240-208-1.us-west-2.compute.internal>
2024-05-05 18:45:39 +0000

    Update AWS Cognito Identity Pool ID

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,6 +1,6 @@
 const awsExports = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:valid-uuid-replaced-here",
+    "aws_cognito_identity_pool_id": "us-east-1:156f814d-0a84-49bd-b191-d6cedea4f374",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_validIdHere",
     "aws_user_pools_web_client_id": "validClientIdHere",
