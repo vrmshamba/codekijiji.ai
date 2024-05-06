1c0250c738355a43b3f671b3da88bc7124b6ae8f
Ubuntu <ubuntu@ip-10-240-208-1.us-west-2.compute.internal>
2024-05-06 08:58:53 +0000

    Integrate interactive visualizations into DataInsights component

--- src/DataInsights.js
+++ src/DataInsights.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Image, Heading } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
 const DataInsights = () => {
   return (
@@ -11,13 +11,25 @@ const DataInsights = () => {
         <Heading as="h3" size="lg" textAlign="center" mb={3}>
           Language Distribution
         </Heading>
-        <Image src="/histogram_language.png" alt="Language Distribution Histogram" />
+        <iframe
+          src="/histogram_language.html"
+          title="Language Distribution Histogram"
+          width="100%"
+          height="400px"
+          style={{ border: 'none' }}
+        />
       </Box>
       <Box my={5}>
         <Heading as="h3" size="lg" textAlign="center" mb={3}>
           Submission Times vs User ID
         </Heading>
-        <Image src="/scatter_submitted_at_vs_user_id.png" alt="Submission Times vs User ID Scatter Plot" />
+        <iframe
+          src="/scatter_submitted_at_vs_user_id.html"
+          title="Submission Times vs User ID Scatter Plot"
+          width="100%"
+          height="400px"
+          style={{ border: 'none' }}
+        />
       </Box>
     </Box>
   );
